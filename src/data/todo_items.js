0159c88ea9c4a3aaa2ca01ba370e60b6bb039132
Queen Bae <sashakyunghwa@gmail.com>
2018-03-29 12:14:58 -0700

    completed to do list

---
+++ src/data/todo_items.js
@@ -0,0 +1,21 @@
+export default [
+    {
+        title: 'Clean the house',
+        details: 'Clean the inside not the outside'
+    },
+    {
+        title: 'Wash car',
+        details: 'Wash the outside not the inside'
+    },
+    {
+        title: 'Feed the bear',
+        details: 'She\'s really hungry'
+    },
+    {
+        title: 'Go buy groceries',
+        details: 'Go to Wholefoods not Superior'
+    },
+];
+
+
+
